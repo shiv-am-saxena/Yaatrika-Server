d012c9dd271253703e5b7ce6da21e27415ad812c
Shivam Saxena <shivam2003official@gmail.com>
2025-04-28 20:33:54 +0530

    feat: update user authentication route and add token verification endpoint

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import userRouter from './routes/user.route.js';
+import { isLoggedIn } from './middlewares/isLoggedIn.js';
+import { apiResponse } from './utils/apiResponse.js';
 const app = express();
 
 app.use(
@@ -18,5 +20,15 @@ app.get('/', (req: Request, res: Response) => {
 	res.send('Hello hai re baba');
 });
 
-app.use('/user/auth', userRouter);
+app.use('/api/v1/user/auth', userRouter);
+app.get('/verify-token', isLoggedIn, (req: Request, res: Response) => {
+	res
+		.status(200)
+		.json(
+			new apiResponse(
+				200,
+				{user: (req as any).user }
+			)
+		);
+});
 export { app };
